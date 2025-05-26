c15e0b9992f2e30399b026d80a7a97d23b82e19d
apb9883 <Apb9883@gmail.com>
2025-05-26 12:58:05 +0530

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,6 @@
+ let myName=`Ankit Prakash`;
+ console.log(myName);
+ document.querySelector('#nameDisplay').innerText=myName;
+
+ let cost=1500-(1500*10/1500);
+ console.log(cost);
---
+++ index4.js
@@ -0,0 +1,6 @@
+//console.log(5<8);
+//console.log(typeof (6>9));
+console.log(5==5);
+console.log(5===5);
+console.log(5===5.0)
+
